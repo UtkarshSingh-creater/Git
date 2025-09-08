e5dc2dd18b142be7a8bf01d2ad8e8bc5ed68c3a3
[UtkarshSingh-creater] <[utkarsh123000@gmail.com]>
2025-09-08 17:23:26 +0530

    add

--- add.cpp
+++ add.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
- 
-//Your code here
-int a=30;
-int b=20;
-int c=a+b;
-cout<<c;
- 
+
+    // Your code here
+    int a = 30;
+    int b = 70;
+    int c = a + b;
+    cout << c;
+
     return 0;
 }
